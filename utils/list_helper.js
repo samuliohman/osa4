9c2f5942c6eb7d385c963d3c22051ac24729feba
Samuli Öhman <samuli.ohman00@gmail.com>
2023-04-09 18:07:46 +0300

    Tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
   // ...
   return 1
 }
-
+const totalLikes = (blogs) => {
+  const sum = blogs.reduce(
+    (a, b) => a + b.likes,
+    0
+  )
+  return blogs.length === 0 ? 0 : sum
+}
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
