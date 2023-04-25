747bd345205001f0457969cca295dbc7613531f6
Samuli Öhman <samuli.ohman00@gmail.com>
2023-04-25 22:06:28 +0300

    Tehtävä 4.18

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 const errorHandler = require('./utils/middleware.js')
-const usersRouter = require('./controllers/users')
 
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(errorHandler)
 
 
